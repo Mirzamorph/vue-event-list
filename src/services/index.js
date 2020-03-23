abce6b1ad10a304c2b0189a2168f0627336df24f
Rustam <mirzamorf@yandex.ru>
2020-03-24 00:09:13 +0300

    pagination finished

--- src/services/index.js
+++ src/services/index.js
@@ -10,8 +10,8 @@ const apiClient = axios.create({
 })
 
 export default {
-  getEvents() {
-    return apiClient.get('/events')
+  getEvents(page = 1, limit = 3) {
+    return apiClient.get(`/events?_page=${page}&_limit=${limit}`)
   },
   getEvent(id) {
     return apiClient.get('/events/' + id)
