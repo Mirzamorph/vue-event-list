3b85de0010dc7bd4072615bced8ba94555e6ed86
Rustam <mirzamorf@yandex.ru>
2020-03-29 16:06:53 +0300

    made it load with loader P.S. I still search for a way to write it better

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,11 +1,8 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <template v-if="!loading">
-      <EventCard v-for="event in events" :key="event.id" :event="event" />
-      <Pagination v-if="totalCount" :totalPages="totalPages" />
-    </template>
-    <p v-else>Данные грузятся</p>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <Pagination v-if="totalCount" :totalPages="totalPages" />
   </div>
 </template>
 
@@ -13,34 +10,50 @@
 import EventCard from '@/components/EventCard'
 import Pagination from '@/components/Pagination'
 import { mapState } from 'vuex'
+import store from '@/store'
 
 export default {
   components: {
     EventCard,
     Pagination
   },
-  data() {
-    return {
-      perPage: 3,
-      totalCount: null
+  props: {
+    totalCount: {
+      type: Number,
+      required: true
     }
   },
   computed: {
-    ...mapState('event', ['events', 'loading']),
+    ...mapState('event', ['events']),
     totalPages() {
-      return Math.ceil(this.totalCount / this.perPage)
+      return Math.ceil(this.totalCount / 3)
     }
   },
-  mounted() {
-    const currentPage = this.$route.query.page || 1
-    this.$store
+  beforeRouteEnter(to, from, next) {
+    const currentPage = to.query.page || 1
+    store
       .dispatch('event/fetchEvents', {
-        perPage: this.perPage,
+        perPage: 3,
+        page: currentPage
+      })
+      .then(({ data, headers }) => {
+        to.params.totalCount = parseInt(headers['x-total-count'])
+        next(() => {
+          if (!data.length) this.$router.push({ name: '404' })
+        })
+      })
+  },
+  beforeRouteUpdate(to, from, next) {
+    const currentPage = to.query.page || 1
+    store
+      .dispatch('event/fetchEvents', {
+        perPage: 3,
         page: currentPage
       })
       .then(({ data, headers }) => {
         if (!data.length) this.$router.push({ name: '404' })
-        this.totalCount = parseInt(headers['x-total-count'])
+        to.params.totalCount = parseInt(headers['x-total-count'])
+        next()
       })
   }
 }
