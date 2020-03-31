575855b1e138cbe07cd81a24cc198324d734b3b9
Rustam <mirzamorf@yandex.ru>
2020-03-31 23:52:59 +0300

    added select component

---
+++ src/components/partials/Select.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <label>Select a category</label>
+    <select v-model="event.category">
+      <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String
+    }
+  }
+}
+</script>
