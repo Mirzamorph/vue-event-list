6177fec9c6f06df448f00f63e67b20199128acd7
Rustam <mirzamorf@yandex.ru>
2020-03-31 23:52:50 +0300

    added input component

---
+++ src/components/partials/Input.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="field">
+    <label>Title</label>
+    <input v-model="event.title" type="text" placeholder="Add an event title" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String
+    }
+  }
+}
+</script>
