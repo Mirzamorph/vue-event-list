4a0ff5dc5806f7abebd9419bf91b027bffd0e639
Rustam <mirzamorf@yandex.ru>
2020-04-02 11:18:46 +0300

    made global select component

---
+++ src/components/partials/BaseSelect.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <select @input="$emit('input', $event.target.value)">
+      <option v-for="item in items" :key="item">{{ item }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String
+    },
+    value: {
+      type: String
+    },
+    items: {
+      type: Array
+    }
+  }
+}
+</script>
