45b83eb083183f53e190e035c1f6066891976925
Rustam <mirzamorf@yandex.ru>
2020-04-02 11:05:29 +0300

    refactor input partial

---
+++ src/components/partials/BaseInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="field">
+    <label>{{ label }}</label>
+    <input
+      type="text"
+      :placeholder="placeholder"
+      @input="$emit('input', $event.target.value)"
+      :value="value"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String
+    },
+    placeholder: {
+      type: String
+    },
+    value: {
+      type: String
+    }
+  }
+}
+</script>
