cc3fb942f8da903cbf694330f92ea3127548790d
Rustam <mirzamorf@yandex.ru>
2020-04-03 21:03:11 +0300

    added validation to create form

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -6,6 +6,7 @@
         label="Select a category"
         v-model="event.category"
         :items="categories"
+        :class="{ error: $v.event.category.$error }"
       />
 
       <h3>Name & describe your event</h3>
@@ -13,11 +14,13 @@
         label="Title"
         placeholder="Add an event title"
         v-model="event.title"
+        :class="{ error: $v.event.title.$error }"
       />
       <base-input
         label="Description"
         placeholder="Add a description"
         v-model="event.description"
+        :class="{ error: $v.event.description.$error }"
       />
 
       <h3>When is your event?</h3>
@@ -28,8 +31,10 @@
         type="date"
         :min="minimalDate"
         max="2025-01-01"
+        :class="{ error: $v.event.date.$error }"
       />
-      <base-input label="Select a time" v-model="event.time" type="time" />
+      <base-input label="Select a time" v-model="event.time" type="time" 
+        :class="{ error: $v.event.category.$error }"/>
 
       <base-button type="submit">Submit</base-button>
     </form>
@@ -37,8 +42,9 @@
 </template>
 
 <script>
-import NProgress from 'nprogress'
+// import NProgress from 'nprogress'
 import moment from 'moment'
+import { required } from 'vuelidate/lib/validators'
 
 export default {
   data() {
@@ -48,28 +54,38 @@ export default {
       event: {
         id: null,
         title: '',
-        date: '',
         time: '',
+        date: '',
         description: '',
         category: ''
       }
     }
   },
+  validations: {
+    event: {
+      title: { required },
+      time: { required },
+      date: { required },
+      category: { required },
+      description: { required }
+    }
+  },
   methods: {
     createEvent() {
-      NProgress.start()
-      this.$store
-        .dispatch('event/createEvent', this.event)
-        .then(event => {
-          if (!event) return
-          this.$router.push({
-            name: 'event-show',
-            params: { id: event.id }
-          })
-        })
-        .catch(() => {
-          NProgress.done()
-        })
+      this.$v.$touch()
+      // NProgress.start()
+      // this.$store
+      //   .dispatch('event/createEvent', this.event)
+      //   .then(event => {
+      //     if (!event) return
+      //     this.$router.push({
+      //       name: 'event-show',
+      //       params: { id: event.id }
+      //     })
+      //   })
+      //   .catch(() => {
+      //     NProgress.done()
+      //   })
     }
   }
 }
